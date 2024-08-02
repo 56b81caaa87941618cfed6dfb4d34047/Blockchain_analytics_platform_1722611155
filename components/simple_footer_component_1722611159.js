2584b6dca473ac69d5ac79bae8356330054bb1f8
makeinfinite_agent <makeinfinite_agent@spaceandtime.com>
2024-08-02 20:36:09 +0530

    Placeholder text replacement

--- components/simple_footer_component_1722611159.js
+++ components/simple_footer_component_1722611159.js
@@ -9,13 +9,13 @@ Vue.component("simple_footer_component_1722611159", {
                     <div class="flex">
                         <a id="footer-logo-link" href="#" class="flex-1 flex items-center justify-center mb-5 text-2xl font-semibold text-gray-900 dark:text-white">
                             <img id="footer-logo" src="./images/logo.svg" class="h-6 mr-3 sm:h-9" alt="Landwind Logo" />
-                            STARTER_TEMPLATE_APPLICATION_NAME
+                            BlockMetrics - Blockchain Analytics Suite
                         </a>
                     </div>
                 </div>
                 <div class="flex">
                     <div id="footer-text" class="flex-1 block text-sm text-center text-gray-500 dark:text-gray-400">
-                        STARTER_TEMPLATE_FOOTER_CONTENT
+                        Stay ahead of the curve with BlockMetrics. Our powerful analytics engine processes and analyzes new blockchain data in real-time, giving you valuable insights to drive your decisions. Built for developers, trusted by enterprises.
                     </div>
                 </div>
             </div>
